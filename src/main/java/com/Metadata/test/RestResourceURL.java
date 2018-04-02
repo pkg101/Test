518d19073ea190c2b3a90a033d4e11f02c138d72
pkg101 <gupta101.pradeep@gmail.com>
2018-04-02 10:03:11 +0530

    new project

---
+++ src/main/java/com/Metadata/test/RestResourceURL.java
@@ -0,0 +1,25 @@
+package com.Metadata.test;
+
+public class RestResourceURL {
+	static final String baseurl = "/services/data/v42.0/";
+	public static String getSobjectDescribeURL(String objectname)
+	{
+		return baseurl+"sobjects/" + objectname + "/describe";
+	}
+	public static String getToolingQueryURL(String query)
+	{
+		return baseurl+"tooling/query/?q="+query;
+	}
+	public static String getSobjectURL()
+	{
+		return baseurl+"sobjects";
+	}
+	public static String getApexTriggerURL(String id)
+	{
+		return baseurl+"tooling/sobjects/ApexTrigger/"+id;
+	}
+	public static String getUserQueryURL(String query) {
+		// TODO Auto-generated method stub
+		return baseurl+"query/?q="+query;
+	}
+}
